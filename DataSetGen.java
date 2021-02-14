68436d001732f41d1622d22c867da880dadb9e82
Nicomello <70982150+Nicomello@users.noreply.github.com>
2021-02-13 23:28:40 -0500

    Add files via upload
    Lab2

---
+++ DataSetGen.java
@@ -0,0 +1,81 @@
+package application;
+
+/**
+   Computes the average of a set of data values.
+*/
+
+//The Generic class of DataSet.
+
+public class DataSetGen <T extends Measurable>
+
+{
+	private double sum;
+	private int count;
+	private T maximum;
+
+
+
+/**
+
+construct an empty data set.
+
+*/
+
+public DataSetGen()
+
+{
+
+count = 0;
+sum = 0;
+maximum = null;
+
+}
+/**
+Adds a data value to the data set
+@param x is a data value
+*/
+
+public void add(T x)
+
+{
+
+	sum = sum + x.getMeasure();
+
+		if (count==0 || maximum.getMeasure() < x.getMeasure())
+			maximum = x;
+		count++;
+
+}
+
+
+
+
+/**
+Gets the average of the added data
+@return the average or 0 if no data has been added
+*/
+
+public double getAverage()
+
+{
+
+
+if (count ==0) return 0;
+
+	else return sum / count;
+
+
+}
+/**
+	Gets the largest of the added data
+	@return the maximum or 0 if no data has been added
+
+	 */
+
+		public T getMaximum()
+
+		{
+
+			return maximum;
+		}
+}
