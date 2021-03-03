d5c2b7fa778bba2cd68a069eeba17675f11bdfbb
Nicomello <70982150+Nicomello@users.noreply.github.com>
2021-03-03 16:32:47 -0500

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class ArraySum {
+
+	public int sumOfArray(Integer[] a,int index) {
+		
+		if (index <= 0)
+			return 0;
+		else {
+		return sumOfArray(a, index - 1) + a[index - 1];
+			 
+		}
+		
+		
+	}
+}
